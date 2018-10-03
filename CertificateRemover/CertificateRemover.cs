2e4953aeec06b07a7d04328079fc08fc1a6a9eb6
artsiom.kuis <artsiom.kuis@sap.com>
2018-10-03 11:33:10 +0300

    Add project files.

---
+++ CertificateRemover/CertificateRemover.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace CertificateRemover
+{
+    public class CertificateRemover : IDisposable
+    {
+        X509Store store;
+        public CertificateRemover()
+        {
+            store = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+            store.Open(OpenFlags.ReadWrite);
+        }
+
+        public bool TryDelete(string certificate)
+        {
+            X509Certificate2Collection col = store.Certificates.Find(X509FindType.FindBySubjectName, certificate, false);
+
+            if (col.Count == 0)
+                return false;
+
+            foreach (var cert in col)
+            {
+                Console.WriteLine(cert.SubjectName.Name);
+                //store.Remove(cert);
+            }
+
+            return true;
+        }
+
+        public void Dispose()
+        {
+            store.Dispose();
+        }
+    }
+}
---
+++ CertificateRemover/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CertificateRemover
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var certificate = args[0];
+            var remover = new CertificateRemover();
+            CancellationTokenSource cts = new CancellationTokenSource();
+            CancellationToken token = cts.Token;
+
+            Console.WriteLine("Press any key to exit...");
+            Task.Factory.StartNew(() =>
+            {
+                while (true)
+                {
+                    if (token.IsCancellationRequested)
+                    {
+                        token.ThrowIfCancellationRequested();
+                    }
+                    remover.TryDelete(certificate);
+                    Thread.Sleep(1000);
+                }
+            }, token);
+
+            Console.ReadKey();
+            cts.Cancel();
+            remover.Dispose();
+        }
+    }
+}
